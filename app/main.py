ed600c2cd8d20861108e13e51afb43e21dd78bc8
Roshni Kumari <roshnikumari212004@gmail.com>
2025-07-27 02:31:52 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:",
+        
+    )
+
+    st.write("Hello world")
+  
+
+
+if __name__=='___main___':
+    main()
